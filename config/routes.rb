7a95e57a81800fc2084e8aa7b2dbd94185d594fc
EC2 Default User <ec2-user@ip-172-31-4-26.us-east-2.compute.internal>
2022-01-01 16:27:13 +0000

    9finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/help'    => 'static_pages#help'
   get  '/about'   => 'static_pages#about'
   get  '/contact' => 'static_pages#contact'
   get  '/signup'  => 'users#new'
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  delete '/logout'=> 'sessions#destroy'
+  resources :users
 end
